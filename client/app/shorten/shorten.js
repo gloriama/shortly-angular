ac95d1751d92d0dd9174b55c775f50e14a8edbf4
gloriama <gloria.ma@aya.yale.edu>
2015-12-31 17:01:19 -0800

    Passes all shortenController tests

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,9 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.link = {};
+  $scope.addLink = function (link) { //inside shortenControllerSpec, the test doesn't take a parameter..hmm
+    Links.addOne(link);
+  };
+
 });
