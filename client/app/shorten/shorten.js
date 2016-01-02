3bf73ed39b3511b462d307ec746e98ed3068ddf1
gloriama <gloria.ma@aya.yale.edu>
2016-01-02 12:19:31 -0800

    Builds out html for shorten and links pages; includes client-side validation for shorten page

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,12 +1,18 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links, Auth) {
-  // Your code here
   $scope.link = {};
   $scope.addLink = function (link) { //inside shortenControllerSpec, the test doesn't take a parameter..hmm
-    Links.addOne(link);
+    console.log("submitted link on shorten page:", link);
+    //client-side validation of url
+    //if valid:
+      Links.addOne({url: link})
+      .then(function (resp) { //for whatever reason, we only get here if the response is status 200
+        console.log("response to POST to /api/links:", resp);
+      });
+    //otherwise:
+      //print some kind of error to user
   };
-
   $scope.signout = Auth.signout;
 
 });
