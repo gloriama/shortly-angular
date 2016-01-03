d9fdad37da23b5e22987b8cb1b70cdc4e5a1e6d8
gloriama <gloria.ma@aya.yale.edu>
2016-01-02 16:48:46 -0800

    Adds authentication to links and shorten pages; renders links with robust server base url

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,8 +1,15 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links, Auth) {
+.controller('LinksController', function ($scope, $location, Links, Auth) {
   // Your code here
   $scope.data = {};
+  $scope.serverPath = $location.$$protocol + '://' + $location.$$host + ':' + $location.$$port;
+
+  if (!Auth.isAuth()) {
+    $location.path('/signin');
+  }
+
+  //render all links
   Links.getAll().then(function (links) {
     $scope.data.links = links;
   });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,6 +2,12 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links, Auth) {
   $scope.link = {};
+  $scope.serverPath = $location.$$protocol + '://' + $location.$$host + ':' + $location.$$port;
+
+  if (!Auth.isAuth()) {
+    $location.path('/signin');
+  }
+
   $scope.addLink = function (link) { //inside shortenControllerSpec, the test doesn't take a parameter..hmm
     console.log("submitted link on shorten page:", link);
     //client-side validation of url
